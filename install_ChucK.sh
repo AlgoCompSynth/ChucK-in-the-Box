2d602855d506065dce50c0d4b02d2d6c17049a80
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-04-05 20:31:40 -0700

    first draft

--- install_ChucK.sh
+++ install_ChucK.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-source ../set_envars
-echo "Setting Qt version"
-export QT_SELECT=qt6
-
-echo "Defining LOGFILE"
-export LOGFILE=$PWD/Logs/install_miniaudicle.log
+echo "Defining environment variables"
+mkdir --parents $PWD/Logs
+export LOGFILE=$PWD/Logs/install_ChucK.log
+rm --force $LOGFILE
+export CHUCK_VERSION="main"
 
 echo "Installing build dependencies"
 sudo apt-get update -qq
@@ -15,29 +14,29 @@ sudo apt-get update -qq
   >> $LOGFILE 2>&1
 /usr/bin/time sudo apt-get install --yes \
   alsa-utils \
+  apt-file \
   bison \
   build-essential \
   flex \
   libasound2-dev \
   libjack-jackd2-dev \
   libpulse-dev \
-  libqscintilla2-qt6-dev \
-  qt6-base-dev \
-  qt6-wayland-dev \
   libsndfile1-dev \
+  plocate \
   >> $LOGFILE 2>&1
-export PATH=/usr/lib/qt6/bin:$PATH
 
-echo "Cloning repository"
+echo "Cloning repositories"
 pushd /tmp
 rm -fr miniAudicle
 /usr/bin/time git clone --recurse-submodules --branch $CHUCK_VERSION \
   https://github.com/ccrma/miniAudicle.git \
   >> $LOGFILE 2>&1
+popd
+exit
 
 echo ""
 echo "Building ChucK"
-pushd miniAudicle/src/chuck/src
+pushd /tmp/miniAudicle/src/chuck/src
 /usr/bin/time make linux-pulse \
   >> $LOGFILE 2>&1
 echo ""
@@ -48,26 +47,13 @@ popd
 
 echo ""
 echo "Building ChuGins"
-pushd miniAudicle/src/chugins
+pushd /tmp/miniAudicle/src/chugins
 /usr/bin/time make linux \
   >> $LOGFILE 2>&1
 echo ""
 echo "Installing ChuGins"
 sudo make install \
   >> $LOGFILE 2>&1
-popd
-
-echo ""
-echo "Building miniAudicle"
-pushd miniAudicle/src
-/usr/bin/time make linux-pulse \
-  >> $LOGFILE 2>&1
-echo ""
-echo "Installing miniAudicle"
-sudo make install \
-  >> $LOGFILE 2>&1
-popd
-
 popd
 
 echo "Finished"
