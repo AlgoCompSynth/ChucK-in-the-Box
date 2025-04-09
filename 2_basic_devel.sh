0188ee6159c7817a2bb506df636be2cbc4d44306
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-04-09 16:43:23 -0700

    don't need to regenerate locales

--- 2_basic_devel.sh
+++ 2_basic_devel.sh
@@ -7,8 +7,6 @@ mkdir --parents "$PWD/Logs"
 export LOGFILE="$PWD/Logs/1_global_installs.log"
 rm --force $LOGFILE
 
-sudo cp locale.gen /etc/
-sudo locale-gen
 echo "Installing basic development packages"
 export DEBIAN_FRONTEND=noninteractive
 /usr/bin/time sudo apt-get install --assume-yes --no-install-recommends \
