95737166585d52d8575d6244ee66ad3af91e926c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-25 20:13:32 -0700

    add OpenCL

--- 2_install_all.sh
+++ 2_install_all.sh
@@ -5,4 +5,5 @@ set -e
 ./apt_audio_base.sh
 ./linux_dependencies.sh
 ./apt_pkg_db_updates.sh
+./install_opencl.sh
 ./install_chuck.sh
--- linux_dependencies.sh
+++ linux_dependencies.sh
@@ -14,15 +14,24 @@ echo "Installing Linux build dependencies"
 export DEBIAN_FRONTEND=noninteractive
 /usr/bin/time sudo apt-get install -qqy --no-install-recommends \
   bison \
+  clang \
   faust \
   faust-common \
   flex \
   libasound2-dev \
+  libclang-cpp-dev \
+  libclang-dev \
   libfaust2 \
+  libhwloc-dev \
   libsndfile1-dev \
   libssl-dev \
   libtinfo-dev \
   llvm \
+  llvm-dev \
+  ocl-icd-dev \
+  ocl-icd-libopencl1 \
+  ocl-icd-opencl-dev \
+  zlib1g-dev \
   >> $LOGFILE 2>&1
 
 echo "Finished"
