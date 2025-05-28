9d2531600056e43fe5797d58ee8ecdf4bf89aa72
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-27 23:42:53 -0700

    set multi-user target

--- 3_edit_boot_params.sh
+++ 3_edit_boot_params.sh
@@ -21,4 +21,7 @@ cp /boot/firmware/config.txt .
 sudo sed -i '$ a dtoverlay=dwc2' /boot/firmware/config.txt
 diff config.txt /boot/firmware/config.txt || true
 
+echo "Dropping to multi-user.target"
+sudo systemctl set-default multi-user.target
+
 echo "Finished -- reboot!"
