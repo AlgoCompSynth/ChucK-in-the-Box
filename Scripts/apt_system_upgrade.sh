a7e1b89ec65ed8a77116b3dbacf05091f6fd8516
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-06-16 17:00:10 -0700

    more status updates in system upgrade

--- Scripts/apt_system_upgrade.sh
+++ Scripts/apt_system_upgrade.sh
@@ -13,10 +13,13 @@ sudo cp locale.gen /etc/
 sudo locale-gen
 
 export DEBIAN_FRONTEND=noninteractive
+echo "Update"
 sudo apt-get update \
   >> $LOGFILE 2>&1
+echo "Full upgrade"
 sudo apt-get full-upgrade --assume-yes \
   >> $LOGFILE 2>&1
+echo "Autoremove"
 sudo apt-get autoremove --assume-yes \
   >> $LOGFILE 2>&1
 
