62a7b76bfe59e4ed0cc436b480ca135adc23dc26
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-08-18 11:10:26 -0700

    terminal setup

---
+++ terminal-setup/terminal-setup.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "*** Terminal Setup ***"
+
+echo "Creating \$HOME/Projects, \$HOME/Logfiles, \$HOME/.local/bin, and \$HOME/bin"
+mkdir --parents $HOME/Projects $HOME/Logfiles $HOME/.local/bin $HOME/bin
+
+echo "Setting base configuration files"
+cp bash_aliases $HOME/.bash_aliases; source bash_aliases
+cp vimrc $HOME/.vimrc
+
+echo "Installing Starship"
+./starship.sh
+
+echo "Installing 'nerd fonts'"
+./nerd_fonts.sh
+
+echo "*** Finished Terminal Setup ***"
