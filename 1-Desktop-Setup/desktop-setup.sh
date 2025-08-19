43d6f23e8d4143f3a991b6cd6ef00334fcb86413
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-08-19 14:23:15 -0700

    cleanup and ditch Flatpak!!

--- 1-Desktop-Setup/desktop-setup.sh
+++ 1-Desktop-Setup/desktop-setup.sh
@@ -15,10 +15,23 @@ echo "Setting base configuration files"
 cp bash_aliases $HOME/.bash_aliases; source bash_aliases
 cp vimrc $HOME/.vimrc
 
+echo "Upgrading system"
+sudo apt-get update \
+  >> $LOGFILE 2>&1
+# https://debian-handbook.info/browse/stable/sect.automatic-upgrades.html
+yes '' \
+  | sudo apt-get -y \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold" \
+  dist-upgrade \
+  >> $LOGFILE 2>&1
+
 echo "Installing utilities" | tee --append $LOGFILE
 sudo apt-get install --assume-yes \
   apt-file \
+  file \
   git \
+  lsb-release \
   plocate \
   qpwgraph \
   tilix \
@@ -28,12 +41,44 @@ sudo apt-get install --assume-yes \
   >> $LOGFILE 2>&1
 
 echo "Installing Starship"
-./starship.sh
+# https://starship.rs/guide/#%F0%9F%9A%80-installation
+pushd /tmp > /dev/null
+  export BIN_DIR=$HOME/.local/bin
+  rm --force install.sh
+  curl --silent --show-error --remote-name https://starship.rs/install.sh
+  chmod +x install.sh
+  ./install.sh --yes \
+    >> $LOGFILE 2>&1
+popd > /dev/null
+mkdir --parents $HOME/.config
+cp starship.toml $HOME/.config/starship.toml
+echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
 
 echo "Installing 'nerd fonts'"
-./nerd_fonts.sh
+pushd /tmp > /dev/null
+  rm --force --recursive Meslo*
+  curl -sOL \
+    https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
+  mkdir Meslo
+  cd Meslo
+  unzip -qq ../Meslo.zip
+  mkdir --parents $HOME/.fonts
+  cp *.ttf $HOME/.fonts
+popd > /dev/null
 
 echo "Installing container hosting"
-./podman-distrobox-hosting.sh
+sudo apt-get install --assume-yes \
+  podman \
+  uidmap \
+  >> $LOGFILE 2>&1
+
+pushd $HOME/Projects > /dev/null
+  rm -fr distrobox
+  git clone https://github.com/89luca89/distrobox.git \
+    >> $LOGFILE 2>&1
+  cd distrobox
+  sudo ./install \
+    >> $LOGFILE 2>&1
+popd > /dev/null
 
 echo "*** Finished Desktop Setup ***" | tee --append $LOGFILE
