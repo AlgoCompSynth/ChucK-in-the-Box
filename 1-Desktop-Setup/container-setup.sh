43250288e0d03813ce9931f1825b1d73b8e8b3e1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-08-19 18:25:57 -0700

    container setup done!

---
+++ 1-Desktop-Setup/container-setup.sh
@@ -0,0 +1,90 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "*** Container Setup ***"
+
+source ../set_envars.sh
+export LOGFILE=$LOGFILES/container-setup.log
+rm --force $LOGFILE
+
+if [[ "$(distrobox list | grep $DBX_CONTAINER_NAME | wc -l)" -gt "0" ]]
+then
+  while true; do
+    echo ""
+    echo "Container $DBX_CONTAINER_NAME exists."
+    read -p "Do you want to force-remove it (y/n)?" yn
+    case $yn in
+      [Yy]* )
+        distrobox rm --force $DBX_CONTAINER_NAME
+        break
+        ;;
+      [Nn]* ) 
+        echo ""
+        echo "Container $DBX_CONTAINER_NAME and container"
+        echo "home directory $DBX_CONTAINER_CUSTOM_HOME"
+        echo "will not be touched!"
+        echo ""
+        echo "*** Finished Container Setup ***" | tee --append $LOGFILE
+        exit
+        ;;
+      * ) echo "Please answer yes or no."
+        ;;
+    esac
+  done
+fi
+
+if [[ -d $DBX_CONTAINER_CUSTOM_HOME ]]
+then
+  while true; do
+    echo ""
+    echo "Container home directory $DBX_CONTAINER_CUSTOM_HOME exists."
+    read -p "Do you want to recursively force-remove it (y/n)?" yn
+    case $yn in
+      [Yy]* )
+        rm --force --recursive $DBX_CONTAINER_CUSTOM_HOME
+        break
+        ;;
+      [Nn]* ) 
+        break
+        ;;
+      * ) echo "Please answer yes or no."
+        ;;
+    esac
+  done
+fi
+
+echo ""
+echo "Creating Distrobox container $DBX_CONTAINER_NAME"
+echo "with home directory $DBX_CONTAINER_CUSTOM_HOME"
+/usr/bin/time distrobox create \
+  --pull \
+  --additional-packages "apt-file file git lsb-release plocate time tree vim" \
+  --additional-packages "starship fonts-font-awesome fonts-material-design-icons-iconfont fonts-weather-icons" \
+  >> $LOGFILE 2>&1
+
+echo ""
+echo "Entering $DBX_CONTAINER_NAME to install additional packages."
+echo "This will take some time."
+distrobox enter --no-tty $DBX_CONTAINER_NAME -- echo "Exiting container!"
+
+echo ""
+echo "Distrobox containers:"
+distrobox list
+
+echo ""
+echo "Setting up container command line"
+cp bash_aliases $DBX_CONTAINER_CUSTOM_HOME/.bash_aliases
+cp vimrc $DBX_CONTAINER_CUSTOM_HOME/.vimrc
+mkdir --parents $DBX_CONTAINER_CUSTOM_HOME/.config
+cp starship.toml $DBX_CONTAINER_CUSTOM_HOME/.config/starship.toml
+echo 'eval "$(starship init bash)"' >> $DBX_CONTAINER_CUSTOM_HOME/.bashrc
+
+echo ""
+echo "To use the container, type"
+echo ""
+echo "  distrobox enter $DBX_CONTAINER_NAME"
+echo ""
+
+echo "*** Finished Container Setup ***" | tee --append $LOGFILE
--- set_envars.sh
+++ set_envars.sh
@@ -2,8 +2,13 @@ echo "Setting environment variables"
 export CHUCK_VERSION="chuck-1.5.5.2"
 export CHUGL_VERSION="main"
 export MAKE_PARALLEL_LEVEL=$(nproc)
-#export MAKE_PARALLEL_LEVEL=1 # for low-RAM systems
+#export MAKE_PARALLEL_LEVEL=1 # use this if needed on low-RAM systems
 export LOGFILES=$HOME/Logfiles
 export DEBIAN_FRONTEND=noninteractive
 export CHUGIN_PATH=/usr/local/lib/chuck
-export LLVM_VERSION=14
+export LLVM_VERSION=19
+
+export DBX_CONTAINER_MANAGER="podman"
+export DBX_CONTAINER_IMAGE="docker.io/library/debian:trixie-backports"
+export DBX_CONTAINER_NAME="ChucK-in-the-Box"
+export DBX_CONTAINER_CUSTOM_HOME="$HOME/$DBX_CONTAINER_NAME-Home"
