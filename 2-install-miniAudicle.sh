e05808e2da32ca356f769b48d698799bda6f3ea8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-09-06 12:40:50 -0700

    streamlining for Pi 4 GB and up

---
+++ 2-install-miniAudicle.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "* miniAudicle *"
+
+source ./set_envars.sh
+export LOGFILE=$LOGFILES/miniaudicle.log
+echo "LOGFILE: $LOGFILE"
+rm --force $LOGFILE
+
+echo "Installing Linux dependencies"
+/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
+  bison \
+  flex \
+  libasound2-dev \
+  libcanberra-gtk3-module \
+  libqscintilla2-qt6-dev \
+  libsndfile1-dev \
+  qt6-base-dev \
+  qt6-base-dev-tools \
+  qt6-wayland \
+  >> $LOGFILE 2>&1
+
+echo "Setting Qt version"
+export QT_SELECT=qt6
+export PATH=/usr/lib/qt6/bin:$PATH
+
+echo "Recursively cloning miniAudicle repository - this takes some time" | tee --append $LOGFILE
+pushd $PROJECTS > /dev/null
+  rm -fr miniAudicle
+  /usr/bin/time git clone \
+    --recurse-submodules \
+    --branch $CHUCK_VERSION \
+    https://github.com/ccrma/miniAudicle.git \
+    >> $LOGFILE 2>&1
+popd > /dev/null
+
+pushd $CHUCK_PATH > /dev/null
+  echo "Building ChucK" | tee --append $LOGFILE
+  /usr/bin/time make --jobs=$MAKE_PARALLEL_LEVEL linux-alsa \
+    >> $LOGFILE 2>&1
+  echo "" | tee --append $LOGFILE
+  echo "Installing ChucK" | tee --append $LOGFILE
+  sudo make install \
+    >> $LOGFILE 2>&1
+popd > /dev/null
+chuck --probe | tee --append $LOGFILE
+
+pushd $CHUGINS_PATH > /dev/null
+  echo "" | tee --append $LOGFILE
+  echo "Building ChuGins" | tee --append $LOGFILE
+  /usr/bin/time make --jobs=$MAKE_PARALLEL_LEVEL linux \
+    >> $LOGFILE 2>&1
+  echo "" | tee --append $LOGFILE
+  echo "Installing ChuGins" | tee --append $LOGFILE
+  sudo make install \
+    >> $LOGFILE 2>&1
+popd > /dev/null
+chuck --chugin-probe | tee --append $LOGFILE
+
+pushd $MINIAUDICLE_PATH > /dev/null
+  echo "" | tee --append $LOGFILE
+  echo "Building miniAudicle" | tee --append $LOGFILE
+  /usr/bin/time make --jobs=$MAKE_PARALLEL_LEVEL linux-alsa \
+    >> $LOGFILE 2>&1
+  echo "" | tee --append $LOGFILE
+  echo "Installing miniAudicle" | tee --append $LOGFILE
+  sudo make install \
+    >> $LOGFILE 2>&1
+popd > /dev/null
+
+echo "* Finished miniAudicle *" | tee --append $LOGFILE
--- set_envars.sh
+++ set_envars.sh
@@ -2,32 +2,21 @@
 export CHUCK_VERSION=chuck-1.5.5.2
 
 ## you shouldn't need to change anything below here
-export ARCH=$(uname -m)
-export DISTRIBUTOR_ID=$(lsb_release --id --short 2> /dev/null)
-export CODENAME=$(lsb_release --codename --short 2> /dev/null)
-
-export RAM_KBYTES=$(grep 'MemTotal:' /proc/meminfo | sed 's/MemTotal:  *//' | sed 's/ .*$//')
-if [[ "$RAM_KBYTES" -gt "3500000" ]]
+if [[ "$(aplay -l | grep pisound | wc -l)" -gt "0" ]]
 then
-  export CMAKE_PARALLEL_LEVEL=$(nproc)
+  export BLOKAS_PISOUND=1
 
 else
-  export CMAKE_PARALLEL_LEVEL=1 # use this if needed on low-RAM systems
-
-fi
-export MAKE_PARALLEL_LEVEL=$CMAKE_PARALLEL_LEVEL
-
-if [[ "$RAM_KBYTES" -le "3500000" || "$(nproc)" -lt "4" ]]
-then
-  export LOW_CAPACITY_SYSTEM=1
-
-else
-  export LOW_CAPACITY_SYSTEM=0
+  export BLOKAS_PISOUND=0
 
 fi
 
 export LOGFILES=$HOME/Logfiles; mkdir --parents $LOGFILES
 export PROJECTS=$HOME/Projects; mkdir --parents $PROJECTS
 export LOCALBIN=$HOME/.local/bin; mkdir --parents $LOCALBIN
-export CHUGIN_PATH=/usr/local/lib/chuck
+export MINIAUDICLE_URL=https://github.com/ccrma/miniAudicle.git
+export MINIAUDICLE_PATH=$PROJECTS/miniAudicle/src
+export CHUCK_PATH=$MINIAUDICLE_PATH/chuck/src
+export CHUGINS_PATH=$MINIAUDICLE_PATH/chugins
 export DEBIAN_FRONTEND=noninteractive
+export MAKE_PARALLEL_LEVEL=$(nproc)
