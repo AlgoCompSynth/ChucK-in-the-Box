2f1956da46cfb7505892c52260753f72e25f889a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-09-13 21:15:06 -0700

    fix DietPi bugs

--- optional-terminal-enhancements/1-terminal-setup.sh
+++ optional-terminal-enhancements/1-terminal-setup.sh
@@ -6,6 +6,7 @@ echo ""
 echo "* Terminal Setup *"
 
 echo "Setting base configuration files"
+echo "source $HOME/.bash_aliases" >> $HOME/.bashrc
 cp bash_aliases $HOME/.bash_aliases; source bash_aliases
 cp vimrc $HOME/.vimrc
 
