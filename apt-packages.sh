edf6b4887947ef84bb621f9892d724e6469a1c94
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-09-15 18:04:03 -0700

    swithcing to ChucK and Faust from distro

---
+++ apt-packages.sh
@@ -0,0 +1,64 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "** Apt Packages **"
+
+source ./set_envars.sh
+export LOGFILE=$LOGFILES/apt-packages.log
+echo "LOGFILE: $LOGFILE"
+rm --force $LOGFILE
+
+echo "Upgrading system" | tee --append $LOGFILE
+# https://debian-handbook.info/browse/stable/sect.automatic-upgrades.html
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -qq \
+  >> $LOGFILE 2>&1
+yes '' | sudo apt-get -qqy \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold" \
+  full-upgrade \
+  >> $LOGFILE 2>&1
+echo "Installing Linux packages" | tee --append $LOGFILE
+sudo apt-get install -qqy --no-install-recommends \
+  apt-file \
+  bash-completion \
+  bluetooth \
+  build-essential \
+  byobu \
+  ca-certificates \
+  cmake \
+  chuck \
+  curl \
+  faust* \
+  file \
+  fluidsynth \
+  fluid-soundfont-gm \
+  fluid-soundfont-gs \
+  git \
+  libfluidsynth-dev \
+  lynx \
+  opl3-soundfont \
+  pipewire-doc \
+  pkg-config \
+  plocate \
+  podman \
+  polyphone \
+  screen \
+  speedtest-cli \
+  time \
+  tmux \
+  tree \
+  uidmap \
+  unzip \
+  usbutils \
+  vim \
+  wget \
+  wireplumber-doc \
+  >> $LOGFILE 2>&1
+
+dpkg-query --list > dpkg-query-list.txt
+
+echo "** Finished Apt Packages **" | tee --append $LOGFILE
+echo ""
