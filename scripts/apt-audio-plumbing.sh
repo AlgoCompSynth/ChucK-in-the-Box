7012a6b24f0bd64cbfa483001064449df29d2b66
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-10-08 23:00:18 -0700

    put pipewire back!

--- scripts/apt-audio-plumbing.sh
+++ scripts/apt-audio-plumbing.sh
@@ -12,10 +12,38 @@ rm --force $LOGFILE
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qqy --no-install-recommends \
+  alsa-topology-conf \
+  alsa-ucm-conf \
+  alsa-utils \
+  bluez-firmware \
+  gstreamer1.0-alsa \
+  libasound2-data \
+  libasound2-plugins \
+  libasound2t64 \
+  libatopology2t64 \
+  libbluetooth3 \
+  libfluidsynth3 \
+  libldacbt-abr2 \
+  libldacbt-enc2 \
+  libpipewire-0.3-0t64 \
+  libpipewire-0.3-modules \
+  libpulse0 \
+  libpulsedsp \
+  libspa-0.2-bluetooth \
+  libspa-0.2-libcamera \
+  libspa-0.2-modules \
+  libwireplumber-0.5-0 \
   pamixer \
+  pipewire \
+  pipewire-bin \
+  pipewire-doc \
+  pipewire-libcamera \
+  pipewire-pulse \
   pulseaudio \
   pulseaudio-module-bluetooth \
   pulseaudio-utils \
+  wireplumber \
+  wireplumber-doc \
   >> $LOGFILE 2>&1
 
 echo "** Finished Audio Plumbing **"
