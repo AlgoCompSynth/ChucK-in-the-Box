5a834f85cd40be4f60b4a331a54dd31f35c570ea
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-11-21 15:17:21 -0800

    add ChuGL to clone script

--- scripts/clone-ccrma-repos.sh
+++ scripts/clone-ccrma-repos.sh
@@ -19,4 +19,17 @@ pushd $PROJECTS > /dev/null
     >> $LOGFILE 2>&1
 popd > /dev/null
 
+echo "Cloning chugl repository" | tee --append $LOGFILE
+pushd $PROJECTS > /dev/null
+  rm -fr chugl
+  /usr/bin/time git clone \
+    https://github.com/ccrma/chugl.git \
+    >> $LOGFILE 2>&1
+
+  echo "Downloading 'lissajous' audio file"
+  cd chugl/examples/basic
+  wget --quiet https://ccrma.stanford.edu/~azaday/music/khrang.wav
+
+popd > /dev/null
+
 echo "** Finished Clone CCRMA Repos **" | tee --append $LOGFILE
