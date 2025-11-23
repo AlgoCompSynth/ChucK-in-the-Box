6784a742f020da84283664a5def0ea543f75e737
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-11-23 13:00:07 -0800

    Container hosting for PiOS

---
+++ 2-pios-container-hosting.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "* PiOS Container Hosting *"
+
+mkdir --parents $HOME/Logfiles
+export LOGFILE=$HOME/Logfiles/2-pios-container-hosting.log
+rm --force $LOGFILE
+
+echo ""
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -qqy \
+  podman \
+  uidmap \
+  >> $LOGFILE 2>&1
+
+./distrobox.sh
+
+echo "* Finished PiOS Container Hosting *"
